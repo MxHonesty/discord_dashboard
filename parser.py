8b92cd0cdead336db231fe20df31d6f81504766a
Stelian <stoianstelian33@gmail.com>
2020-09-06 16:26:53 +0300

    find html functions

--- parser.py
+++ parser.py
@@ -1,3 +1,10 @@
+def replace_name(nume, html):
+    print(html.replace("$NUME_BOT$", nume))
+
+def replace_server(server, html):
+    print(html.replace("$NUME_SERVER$", server))
+
 with open("index.html") as file:
     text = file.read()
-    print(text)
+    replace_name('Steli', text)
+    replace_server('Server', text)
