bc8a68d0d436b2eb4c387b2cc99f6d65841fc6a1
Stelian <stoianstelian33@gmail.com>
2020-09-09 15:05:02 +0300

    html read functions

--- parser.py
+++ parser.py
@@ -4,10 +4,21 @@ def replace_name(nume, html):
 def replace_server(server, html):
     print(html.replace("$NUME_SERVER$", server))
 
-with open("src/index.html") as file:
-    text = file.read()
-    replace_name('Steli', text)
-    replace_server('Server', text)
+#with open("src/index.html") as file:
+#    text = file.read()
+#    replace_name('Steli', text)
+#    replace_server('Server', text)
 
-with open("src/server_grid.html") as file:
-    print(file.read())
+#with open("src/server_grid.html") as file:
+#    print(file.read())
+
+def read_main_page():
+    with open("src/index.html") as f:
+        return f.read()
+
+def read_server_holder():
+    with open("src/server_grid.html") as f:
+        return f.read()
+
+print(read_main_page())
+print(read_server_holder())
